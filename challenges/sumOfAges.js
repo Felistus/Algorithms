e25fe17e120af6088e3075044799b7dfc327bcb0
felistus <felistusobieze@gmail.com>
2022-07-10 04:07:05 +0100

    sumOfAges algorithm

---
+++ challenges/sumOfAges.js
@@ -0,0 +1,14 @@
+// Find the sum of all the ages in an array of objects
+// For example;
+// [{name: ‘Uche Egbo’, age: 24, occupation: ‘Trader’}, {name: ‘Solomon Ogbodo’, age: 30, occupation: ‘Accountant’}]
+// to return 54.
+
+function sumOfAges(array) {
+  return array.map((value) => value.age).reduce((a, b) => a + b);
+}
+console.log(
+  sumOfAges([
+    { name: "Uche Egbo", age: 54, occupation: "Trader" },
+    { name: "Solomon Ogbodo", age: 30, occupation: "Accountant" },
+  ])
+);
