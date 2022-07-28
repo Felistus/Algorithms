c88dde769fdd927e8f19d64b046ca1b40a47c404
felistus <felistusobieze@gmail.com>
2022-07-28 18:23:51 +0100

    added stack and queue data structures

---
+++ queue.js
@@ -0,0 +1,39 @@
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+class Queue {
+  constructor() {
+    this.first = null;
+    this.last = null;
+    this.size = 0;
+  }
+  // push (adds new items from the back of the queue)
+  push(element) {
+    let newNode = new Node(element);
+    if (!this.size) this.first = this.last = newNode;
+    else {
+      this.last.next = newNode;
+      this.last = newNode;
+    }
+    this.size++;
+    return this;
+  }
+  // shift (deletes item from the front of the stack)
+  pop() {
+    let oldNode = this.first;
+    if (!this.size) return undefined;
+    if (this.size === 1) this.first = this.last = null;
+    else {
+      this.first = oldNode.next;
+    }
+    this.size--;
+    return oldNode;
+  }
+}
+
+const queue = new Queue();
+queue.push(100);
+queue.push(200);
---
+++ stack.js
@@ -0,0 +1,40 @@
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class Stack {
+  constructor() {
+    this.first = null;
+    this.last = null;
+    this.size = 0;
+  }
+  // unshift (adds new items to the front of the stack)
+  push(element) {
+    let newNode = new Node(element);
+    if (!this.size) this.first = this.last = newNode;
+    else {
+      newNode.next = this.first;
+      this.first = newNode;
+    }
+    this.size++;
+    return this;
+  }
+  // shift (deletes item from the front of the stack)
+  pop() {
+    let removedHead = this.first;
+    if (!this.size) return undefined;
+    if (this.size === 1) this.first = this.last = null;
+    else {
+      this.first = removedHead.next;
+    }
+    this.size--;
+    return removedHead;
+  }
+}
+
+const stack = new Stack();
+stack.push(100);
+stack.push(200);
